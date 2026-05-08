2d020d4e7c27b179ca75b3ab64a8cda077790384
manishvishvkarma89 <manish.kumar.v@ramanujan.du.ac.in>
2026-05-09 01:39:33 +0530

    automate terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,10 @@ terraform {
 }
 locals {
 
-  region          = "eu-north-1"
+  region          = "eu-west-1"
   name            = "erp-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["eu-north-1a", "eu-north-1b","eu-north-1c"]
+  azs             = ["eu-west-1a", "eu-west-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
